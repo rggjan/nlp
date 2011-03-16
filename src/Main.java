7facea3511d4cd9f8d916d84b5de95b943a14db1
Jan Rüegg <rggjan@gmail.com>
2011-03-16 12:32:19 +0100

    + Added real prefix detection (a little bit messy still...)

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,39 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println("Hello World\n");
-
 		HashMap<String, String> prefixes = new HashMap<String, String>();
+		HashMap<String, Boolean> prefix_found = new HashMap<String, Boolean>();
 		
-		String text = "This is a sentence";
+		String text = "house inhouse insist interesting";
 		for (String word : text.split(" ")) {
-			for (int i=0; i<4; i++) {
+			for (int i=4; i>=1; i--) {
 				try {
 					String prefix = word.substring(0, i);
 					String stem = word.substring(i, word.length());
-
-					System.out.println(prefix + "/" + stem);
+					
+					if (stem.length() > 0) {
+						if (prefix_found.get(prefix) != null) {
+							System.out.println(prefix + "/" + stem);
+							break;
+						} else {
+							if (prefixes.get(prefix) != null && prefixes.get(prefix) != stem) {
+								System.out.println(prefix + "/" + prefixes.get(prefix));
+								System.out.println(prefix + "/" + stem);
+								
+								prefix_found.put(prefix, true);
+								break;
+							} else {
+								prefixes.put(prefix, stem);
+							}
+						}
+					}
 				} catch (java.lang.StringIndexOutOfBoundsException exception) {
 					// This happens with short strings...
 				}
 			}
 		}
+		
+		System.exit(0);
 		
 		// create alphabets
 		Alphabet lexicalAlphabet=new Alphabet();
