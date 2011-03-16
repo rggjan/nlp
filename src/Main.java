67c29e83ad91d0fccb475c88163eb38c02d79756
Jan Rüegg <rggjan@gmail.com>
2011-03-16 12:18:31 +0100

    + Started implementing prefix parser

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 import java.io.Console;
+import java.util.HashMap;
 
 
 public class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hello World\n");
+
+		HashMap<String, String> prefixes = new HashMap<String, String>();
+		
+		String text = "This is a sentence";
+		for (String word : text.split(" ")) {
+			for (int i=0; i<4; i++) {
+				try {
+					String prefix = word.substring(0, i);
+					String stem = word.substring(i, word.length());
+
+					System.out.println(prefix + "/" + stem);
+				} catch (java.lang.StringIndexOutOfBoundsException exception) {
+					// This happens with short strings...
+				}
+			}
+		}
 		
 		// create alphabets
 		Alphabet lexicalAlphabet=new Alphabet();
