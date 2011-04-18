0cf61340929fbfe4c462ec150a9056ecf63df5a3
Jan Rüegg <rggjan@gmail.com>
2011-04-18 19:30:46 +0200

    + Parse sentences

--- project2/src/hmm/TextParser.java
+++ project2/src/hmm/TextParser.java
@@ -4,23 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class TextParser {
-	String rawText;
+	ArrayList<String> rawText;
+	
+	public TextParser() {
+		rawText = new ArrayList<String>();
+	}
 
 	public void readText(String filename) throws IOException {
 		BufferedReader br;
-		StringBuffer contentOfFile = new StringBuffer();
+		StringBuffer sentence = new StringBuffer();
 
 		br = new BufferedReader(new InputStreamReader(new FileInputStream(
 				filename)));
 
 		String line;
-
-		while ((line = br.readLine()) != null) {
-			contentOfFile.append(" " + line);
+		line = br.readLine();
+		while (line != null) {
+			if (line.trim().length() > 0) {
+				sentence.append(" " + line);
+			} else {
+				rawText.add(sentence.toString());
+				sentence = new StringBuffer();
+			}
+			line = br.readLine();
 		}
-
-		rawText = contentOfFile.toString();
 	}
 }
