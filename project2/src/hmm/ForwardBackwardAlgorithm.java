40f3f597e1bbe9bb63eb8a72d88c30d1af7fb44c
Jan Rüegg <rggjan@gmail.com>
2011-05-06 21:06:04 +0200

    ! Fixed forward backward, start with index 0

--- project2/src/hmm/ForwardBackwardAlgorithm.java
+++ project2/src/hmm/ForwardBackwardAlgorithm.java
@@ -9,82 +9,106 @@ public class ForwardBackwardAlgorithm {
 	private final List<String> outputSencence;
 	private final ArrayList< HashMap<State, Double> > cache;
 	private final Boolean isForwardAlgorithm;
-	
+
 	public ForwardBackwardAlgorithm(final StateCollection hmm, final List<String> output, Boolean isForward) {
 		stateCollection = hmm;
 		outputSencence = output;
-		
+
 		cache = new ArrayList< HashMap<State, Double> >();
 		isForwardAlgorithm = isForward;
 	}
 
 	private void addToCache(int index, State qj, double probability) {
-		HashMap<State, Double> stateCache; 
-		
+		HashMap<State, Double> stateCache;
+
 		stateCache = cache.get(index);
 		if (stateCache == null) {
 			stateCache = new HashMap<State, Double>();
 			cache.set(index, stateCache);
 		}
-			
+
 		stateCache.put(qj, probability);
 	}
-	
+
 	private double checkCache(int index, State qj) {
-		HashMap<State, Double> stateCache; 
-		
+		HashMap<State, Double> stateCache;
+
 		stateCache = cache.get(index);
 		if (stateCache != null)
 			return stateCache.get(qj);
-		
+
 		return -1;
 	}
-	
-	// Get the probability for a certain element at a certain index
-	public double getAlphaBeta(int index, State qj) {
-		assert(index >= 1);
-		assert(index <= outputSencence.size()); 
-		
-		double cachedValue = checkCache(index, qj); 
-		if (cachedValue != -1)
-			return cachedValue;
-			
-		if (isForwardAlgorithm) {
-			if (index == 1) {
-				double probability;
-				probability = stateCollection.startState().nextStateProbability(qj);
-				probability *= qj.wordEmittingProbability(new Word(outputSencence.get(0)));
-				// TODO probably should not create "new" word
-				addToCache(index, qj, probability);
-				return probability;			
-			}
-		} else {
-			if (index == outputSencence.size()) {
-				double probability;
-				probability = qj.nextStateProbability(stateCollection.endState());
-				addToCache(index, qj, probability);
-				return probability;			
-			}
-		}
-		
+
+	public double getFinalProbability() {
+		// TODO cache that function?
+
 		double sum = 0;
-		
+
 		for (State qi : stateCollection.states.values()) {
 			double value;
 			if (isForwardAlgorithm) {
-				value = getAlphaBeta(index - 1, qj);
-				value *= qi.nextStateProbability(qj);
-				value *= qj.wordEmittingProbability(new Word(outputSencence.get(index - 1)));
-				// TODO probably should not create "new" word
+				value = getAlphaBeta(outputSencence.size() - 1, qi);
+				value *= qi.nextStateProbability(stateCollection.endState());
 			} else {
-				value = getAlphaBeta(index + 1, qi);
-				value *= qj.nextStateProbability(qi);
-				value *= qi.wordEmittingProbability(new Word(outputSencence.get(index - 1)));
-				// TODO probably should not create "new" word
+				value = getAlphaBeta(0, qi);
+				value *= stateCollection.startState().nextStateProbability(qi);
+				value *= qi.wordEmittingProbability(stateCollection
+						.getWord(outputSencence.get(0)));
 			}
 			sum += value;
 		}
-		
+		return sum;
+	}
+
+	// Get the probability for a certain element at a certain index
+	// Index "0" state "q3" means: Probability of getting from start state to
+	// q3 and outputting word0
+	public double getAlphaBeta(int index, State qj) {
+		assert (index >= 0);
+		assert (index < outputSencence.size());
+
+		double cachedValue = checkCache(index, qj);
+		if (cachedValue != -1)
+			return cachedValue;
+
+		if (isForwardAlgorithm) {
+			if (index == 0) {
+				double probability;
+				probability = stateCollection.startState().nextStateProbability(qj);
+				probability *= qj.wordEmittingProbability(stateCollection
+						.getWord(outputSencence.get(0)));
+
+				addToCache(index, qj, probability);
+				return probability;
+			}
+		} else {
+			if (index == outputSencence.size() - 1) {
+				double probability;
+				probability = qj.nextStateProbability(stateCollection.endState());
+				addToCache(index, qj, probability);
+				return probability;
+			}
+		}
+
+		double sum = 0;
+
+		for (State qi : stateCollection.states.values()) {
+			double value;
+			if (isForwardAlgorithm) {
+				value = getAlphaBeta(index - 1, qi);
+				value *= qi.nextStateProbability(qj);
+				value *= qj.wordEmittingProbability(stateCollection
+						.getWord(outputSencence.get(index)));
+			} else {
+				value = getAlphaBeta(index + 1, qi);
+				value *= qi.nextStateProbability(qj);
+				value *= qi.wordEmittingProbability(stateCollection
+						.getWord(outputSencence.get(index)));
+			}
+			sum += value;
+		}
+
 		addToCache(index, qj, sum);
 		return sum;
 	}
